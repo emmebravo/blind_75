8038601dd24d26773e4bbd936074b784f589218d
Emme Bravo <75594869+emmebravo@users.noreply.github.com>
2022-03-12 16:55:36 -0800

    Create longest_repeating_character_replacement.js

---
+++ longest_repeating_character_replacement.js
@@ -0,0 +1,44 @@
+/*
+
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+Example 1:
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+Example 2:
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4
+
+*/
+
+const characterReplacement = function(s, k) {
+  const obj = {}
+  let right = 0, left = 0, result = 0, mostCommonLetterCount = 0
+
+  while(right < s.length){
+    obj[s[right]] = obj[s[right]] + 1 || 1
+    
+    if(obj[s[right]] > mostCommonLetterCount) mostCommonLetterCount = obj[s[right]]
+    
+    if(right - left + 1 - mostCommonLetterCount <= k){
+      result = Math.max(result, right - left + 1)
+      right++
+    }else{
+      obj[s[left]] -= 1
+      obj[s[right]] -= 1
+      left++
+    }
+  }
+  return result
+};
+
+
+console.log(characterReplacement("AABABBA", 1), 4)
+console.log(characterReplacement("AABABBA", 2), 5)
+console.log(characterReplacement("ABAB", 2), 4)
